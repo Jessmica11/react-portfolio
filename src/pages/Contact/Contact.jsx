cc8969739c76977af1cbd50b26ca895bbf56c77f
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2024-11-04 16:24:57 -0500

    added space for Contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
   return (
     <section>
       <MetaData title="Contact Me | Jessica Scheck Portfolio" description="Get in touch with Jessica Scheck for collaboration or inquiries." />
-      <p>Once your form is submitted, I will reach out as soon as possible to the email address provided.</p>
+      <p><br />Once your form is submitted, I will reach out as soon as possible to the email address provided.</p>
       <ContactForm />
     </section>
   );

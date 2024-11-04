14696c872154d43b156b71d7f0ca1d0d8fed2282
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2024-11-04 15:36:31 -0500

    fixed error in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
-import Nav from "./components/Nav/Nav";
 import About from "./pages/About/About";
 import ProjectList from "./pages/ProjectList/ProjectList";
 import Contact from "./pages/Contact/Contact";
@@ -14,7 +13,6 @@ function App() {
   return (
     <Router>
       <Header />
-      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
